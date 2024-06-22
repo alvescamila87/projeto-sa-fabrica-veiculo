85ecc9cc37c4d4aa1ecc7a4627e325487dcea0b1
Camila <alvescamila87@gmail.com>
2024-06-22 19:21:48 -0300

    documentation exception in javadoc and regererate doc

--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/exceptions/APIExceptionHandler.java
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/exceptions/APIExceptionHandler.java
@@ -5,9 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * Manipulador global de exceções da API
+ */
 @ControllerAdvice
 public class APIExceptionHandler {
 
+    /**
+     * Manipula exceções do tipo {@link NotFoundException}.
+     * @param ex a exceção a ser manipulada.
+     * @return a resposta de erro com a mensagem da exceção.
+     */
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/exceptions/NotFoundException.java
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/exceptions/NotFoundException.java
@@ -1,7 +1,15 @@
 package br.com.senai.sa2semestre.fabricaveiculo.exceptions;
 
+/**
+ * Exceção lançada quando um recurso não é encontrado.
+ */
 public class NotFoundException extends RuntimeException{
 
+    /**
+     * Constrói uma nova exceção com a mensagem especificada.
+     *
+     * @param mensagem a mensagem detalhada da exceção.
+     */
     public NotFoundException(String mensagem){
         super(mensagem);
     }
